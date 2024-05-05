3ac51bef344b5a107cb3a604f0cf16e1f0b02b3f
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 18:49:52 +0200

    MAIL: JS: ciclo for: verifica e-mails

--- mail/mail_js/mail.js
+++ mail/mail_js/mail.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// CHIEDERE EMAIL UTENTE
+// CHIEDERE E-MAIL UTENTE
 
 const utente = prompt('Inserisci qui la tua mail:');
 
-// LISTA MAILS
+// LISTA E-MAILS
 
 const lista = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com', 'topolino@gmail.com', 'minnie@gmail.com'];
 
@@ -12,6 +12,33 @@ const lista = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com', 'topo
 
 console.log('Accesso: ', lista);
 console.log('Utente: ', utente);
+
+// VERIFICARE LISTA E-MAILS
+
+let verifica = false;
+
+/* poniamo la variabile falsa in partenza, in modo tale da poterla andare a sovrascrivere come vera solo successivamente al controllo che faremo avviando un ciclo di verifica for */
+
+
+
+// CICLO DI VERIFICA FOR
+
+for (let i = 0; i < verifica.lenght; i++)
+
+    /* in questo modo impostiamo un punto di partenza del ciclo (0) ed impostiamo anche un limite, cioè la lunghezza (lenght) della lista di verifica. infine, dal punto 0, il controllo avverrà passo passo (i++) */
+
+    {
+
+        /* con questa condizione andremo a sovrascrivere il valore 'false' di partenza della variabile 'verifica' */
+
+        if (utente === lista[i])
+            {verifica = true; break;}
+        
+        /* con break; andiamo ad interrompere il ciclo poiché abbiamo trovato l'e-mail che coincide con la lista */
+        
+    }
+
+
 
 
 
