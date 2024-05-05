dcb1946f0a70f7cbea35f6bd64a6dc09608c69db
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 18:38:00 +0200

    MAIL: HTML + JS: user and list

---
+++ mail/mail_js/mail.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// CHIEDERE EMAIL UTENTE
+
+const utente = prompt('Inserisci qui la tua mail:');
+
+// LISTA MAILS
+
+const lista = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com', 'topolino@gmail.com', 'minnie@gmail.com'];
+
+// STAMPARE
+
+console.log('Accesso: ', lista);
+console.log('Utente: ', utente);
+
+
+
+
+
+
