c020eee7a080b3106cd55568cd6a9d6cb904a5f1
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 19:07:16 +0200

    DADI: JS: lancio utente + lancio computer

--- dadi/dadi_js/dadi.js
+++ dadi/dadi_js/dadi.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// UTENTE NUMERO RANDOM 1-6 
+const tiraU = Math.floor((Math.random() * 6) +1);
+console.log('Risultato: ', tiraU);
+
+// COMPUTER NUMERO RANDOM 1-6
+
+const tiraC = Math.floor((Math.random() * 6) +1);
+console.log('Risultato: ', tiraC);
+
+
+
